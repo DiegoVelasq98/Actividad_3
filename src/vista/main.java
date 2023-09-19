8617fd6545e7c140ceb3ee349d092b6dc0b2694f
DiegoVelasq98 <89673799+DiegoVelasq98@users.noreply.github.com>
2023-09-18 21:10:26 -0600

    Actividad 3

---
+++ src/vista/main.java
@@ -0,0 +1,20 @@
+
+package vista;
+
+import modelo.conexion;
+
+public class main {
+
+   
+    public static void main(String[] args) {
+
+//frm_personas frm =new frm_personas();
+     // frm.show();
+        
+      
+      frm_empleados frm =new frm_empleados();
+      frm.show();
+ 
+    }
+    
+}
